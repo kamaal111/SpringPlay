e57ffc4d1f63f8273f8c9447935da40329bdc0b5
Kamaal <kamaal.f1@gmail.com>
2022-11-22 21:45:00 +0100

    Initial commit

---
+++ Makefile
@@ -0,0 +1,3 @@
+run:
+	./gradlew build
+	./gradlew bootRun
---
+++ src/test/java/io/kamaal/SpringPlay/SpringPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kamaal.SpringPlay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
